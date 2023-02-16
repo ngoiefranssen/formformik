e0d5e9ffe32f231570e86a7d1cc09171b7816625
Franssen <ngoiefranssen04@gmail.com>
2023-02-16 11:38:01 +0200

    Wip

--- src/components/muiForm.jsx
+++ src/components/muiForm.jsx
@@ -17,6 +17,25 @@ const validate =(values) => {
     // errors.name = "This field is required"
 
     var errors = {}
+
+    if(!values.name){
+        errors.name = "Required"
+    }
+    if(!values.lastname){
+        errors.lastname = "Required"
+    }
+    if(!values.email){
+        errors.email = "Required"
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+      errors.email = "Invalid email format"  
+    }
+    if(!values.mobile){
+        errors.mobile = "Required"
+    }else if(mobile.length >= 9){
+        errors.mobile = "Minimum 10 numbers required"
+    }
+
+    return errors
 }
 const muiForm = () => {
   return (
